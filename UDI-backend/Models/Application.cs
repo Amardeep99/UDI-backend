ca1acb9b865af477e0a1874ad509355357df1151
Amardeep99 <amardeep@hotmail.no>
2024-10-10 13:37:12 +0200

    test

--- UDI-backend/Models/Application.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UDI_backend.Models {
-	public class Application {
-		public int ApplicationID { get; set; } // PK
-		public int D_number {  get; set; }
-		public DateTime TravelDate { get; set; }
-
-	}
-}
