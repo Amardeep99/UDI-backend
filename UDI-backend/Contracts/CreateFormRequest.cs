6dc2781c487b15b1fd14f8655ca16751fe27342b
Amardeep99 <amardeep@hotmail.no>
2024-10-13 19:56:57 +0200

    Add Json instead of query

---
+++ UDI-backend/Contracts/CreateFormRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace UDI_backend.Contracts {
+	public class CreateFormRequest {
+		public int ReferenceId { get; set; }
+
+		public bool HasObjection { get; set; }
+
+		public string ObjectionReason { get; set; } = "";
+
+		public bool HasDebt { get; set; }
+
+		public int OrganisationNr { get; set; }
+
+		public string OrganisationName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string Phone { get; set; } = null!;
+
+		public string ContactName { get; set; } = null!;
+
+	}
+}
